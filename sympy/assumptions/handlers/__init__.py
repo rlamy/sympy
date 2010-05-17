99257cb8a59d9b0df9c5e1b9ab5181e9f811b664
Ronan Lamy <Ronan.Lamy@normalesup.org>
2010-05-17 20:09:59 +0100

    Use refine_logic in handlers

--- sympy/assumptions/handlers/__init__.py
+++ sympy/assumptions/handlers/__init__.py
@@ -1,5 +1,5 @@
 from sympy.logic.boolalg import conjuncts
-from sympy.assumptions import Q, ask
+from sympy.assumptions import Q, ask, refine_logic
 
 class AskHandler(object):
     """Base class that all Ask Handlers must inherit"""
@@ -51,51 +51,8 @@ class TautologicalHandler(AskHandler):
 
     @staticmethod
     def ApplyPredicate(expr, assumptions):
-        return ask(expr.arg, expr.func, assumptions)
+        return (expr, assumptions)
 
     @staticmethod
-    def Not(expr, assumptions):
-        value = ask(expr.args[0], Q.is_true, assumptions=assumptions)
-        if value in (True, False):
-            return not value
-        else:
-            return None
-
-
-    @staticmethod
-    def Or(expr, assumptions):
-        result = False
-        for arg in expr.args:
-            p = ask(arg, Q.is_true, assumptions=assumptions)
-            if p == True:
-                return True
-            if p == None:
-                result = None
-        return result
-
-    @staticmethod
-    def And(expr, assumptions):
-        result = True
-        for arg in expr.args:
-            p = ask(arg, Q.is_true, assumptions=assumptions)
-            if p == False:
-                return False
-            if p == None:
-                result = None
-        return result
-
-    @staticmethod
-    def Implies(expr, assumptions):
-        p, q = expr.args
-        return ask(~p | q, Q.is_true, assumptions=assumptions)
-
-    @staticmethod
-    def Equivalent(expr, assumptions):
-        p, q = expr.args
-        pt = ask(p, Q.is_true, assumptions=assumptions)
-        if pt == None:
-            return None
-        qt = ask(q, Q.is_true, assumptions=assumptions)
-        if qt == None:
-            return None
-        return pt == qt
+    def BooleanFunction(expr, assumptions):
+        return expr.func(*[refine_logic(arg, assumptions) for arg in expr.args])
