154886e847d4ed044b3f133e8b63bc261f0a2e40
Ronan Lamy <Ronan.Lamy@normalesup.org>
2011-05-09 18:04:23 +0100

    Transfer LatticeOp to sympy.core.containers
    * 'test_operations.py' merged with 'test_containers.py', since it only deals with LatticeOp

--- sympy/core/tests/test_containers.py
+++ sympy/core/tests/test_containers.py
@@ -1,7 +1,10 @@
-from sympy import Matrix, Tuple, symbols, sympify, Basic, Dict, S, FiniteSet
-from sympy.core.containers import tuple_wrapper
+from sympy import Matrix, Tuple, symbols, sympify, Basic, Integer, S
+from sympy.core.containers import Tuple, tuple_wrapper, LatticeOp
+from sympy.core.sympify import SympifyError
+
 from sympy.utilities.pytest import raises
 from sympy.core.compatibility import is_sequence, iterable
+
 
 def test_Tuple():
     t = (1, 2, 3, 4)
@@ -120,3 +123,33 @@ def issue_2689():
         pair = [o(*args), o(*reversed(args))]
         assert sorted(pair) == sorted(reversed(pair))
         assert set(o(*args)) # doesn't fail
+
+# create the simplest possible Lattice class
+class join(LatticeOp):
+    zero = Integer(0)
+    identity = Integer(1)
+
+def test_lattice_simple():
+    assert join(join(2, 3), 4) == join(2, join(3, 4))
+    assert join(2, 3) == join(3, 2)
+    assert join(0, 2) == 0
+    assert join(1, 2) == 2
+    assert join(2, 2) == 2
+
+    assert join(join(2, 3), 4) == join(2, 3, 4)
+    assert join() == 1
+    assert join(4) == 4
+    assert join(1, 4, 2, 3, 1, 3, 2) == join(2, 3, 4)
+
+def test_lattice_shortcircuit():
+    raises(SympifyError, 'join(object)')
+    assert join(0, object) == 0
+
+def test_lattice_print():
+    assert str(join(5, 4, 3, 2)) == 'join(2, 3, 4, 5)'
+
+def test_lattice_make_args():
+    assert join.make_args(0) == set([0])
+    assert join.make_args(1) == set([1])
+    assert join.make_args(join(2, 3, 4)) == set([S(2), S(3), S(4)])
+
