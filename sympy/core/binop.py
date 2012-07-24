a02d653401ac30ad2a126be7a527fd10251cc5a5
Ronan Lamy <Ronan.Lamy@normalesup.org>
2012-07-24 18:11:44 +0100

    binop: handle conflicting definitions

--- sympy/core/binop.py
+++ sympy/core/binop.py
@@ -2,6 +2,9 @@
 Module implementing binary operations with double dispatch.
 """
 
+class ConflictingDefinitions(TypeError):
+    pass
+
 class binary_operation(object):
     def __init__(self):
         self.default = None
@@ -15,9 +18,23 @@ class binary_operation(object):
 
     def __getitem__(self, types):
         x_type, y_type = types
+        candidates = set()
         for left in x_type.mro():
             for right in y_type.mro():
-                try:
-                    return self.dispatcher[left, right]
-                except KeyError:
-                    pass
+                if (left, right) not in self.dispatcher:
+                    continue
+                for c_left, c_right in candidates:
+                    if issubclass(c_left, left) and issubclass(c_right, right):
+                        break
+                else:
+                    for c_left, c_right in list(candidates):
+                        if issubclass(left, c_left) and issubclass(right, c_right):
+                            candidates.remove((c_left, c_right))
+                    candidates.add((left, right))
+        funcs = set(self.dispatcher[c] for c in candidates)
+        if len(funcs) == 1:
+            return funcs.pop()
+        elif not candidates:
+            return self.default
+        else:
+            raise ConflictingDefinitions(candidates)
--- sympy/core/tests/test_binop.py
+++ sympy/core/tests/test_binop.py
@@ -1,6 +1,6 @@
 from sympy.utilities.pytest import raises
 
-from sympy.core.binop import binary_operation
+from sympy.core.binop import binary_operation, ConflictingDefinitions
 
 def test_binop():
     mul = binary_operation()
@@ -16,3 +16,26 @@ def test_binop():
     assert isinstance(mul(Ring(), Field()), Ring)
     with raises(TypeError):
         mul(Monoid(), Field())
+
+def test_conflict():
+    op = binary_operation()
+    class Parent(object): pass
+    class Child(Parent): pass
+    op[Parent, Child] = lambda x,y: 1
+    op[Child, Parent] = lambda x,y: 2
+
+    assert op(Parent(), Child()) == 1
+    assert op(Child(), Parent()) == 2
+    with raises(ConflictingDefinitions):
+        op(Child(), Child())
+
+def test_no_conflict():
+    op = binary_operation()
+    class Parent(object): pass
+    class Child(Parent): pass
+    op[Parent, Child] = lambda x,y: 1
+    op[Child, Parent] = op[Parent, Child]
+
+    assert op(Parent(), Child()) == 1
+    assert op(Child(), Parent()) == 1
+    assert op(Child(), Child()) == 1
