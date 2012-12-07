f08f295a59e3d2e9587bd40f35ad281249ab376b
Ronan Lamy <Ronan.Lamy@normalesup.org>
2012-12-07 05:47:01 +0000

    Use xreplace in subs()

--- sympy/rules/tools.py
+++ sympy/rules/tools.py
@@ -15,4 +15,4 @@ def subs(d):
     >>> subs(mapping)(expr)
     Basic(4, Basic(2, 3), Basic(1, Basic(6, 7)))
     """
-    return top_down(do_one(*map(rl.subs, *zip(*d.items()))))
+    return lambda expr: expr.xreplace(d)
